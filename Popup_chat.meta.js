a77434b3580cbe763c3d5129f2076af9073bf4dc
breakerh <info@hammer.bz>
2014-08-22 09:08:23 +0200

    Create Popup_chat.meta.js

---
+++ Popup_chat.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Popup Chat
+// @namespace   ssm
+// @description Instead of an in screen chat, pop it allllll up!
+// @include     http://*.grepolis.*/game*
+// @version     0.3.2
+// @grant       none
+// @downloadURL https://www.slingshotmedia.nl/Popup_chat.user.js
+// @updateURL https://www.slingshotmedia.nl/Popup_chat.meta.js
+// ==/UserScript==
