86529fd7d7839161d6bb15acd7192d24d1f9af20
breakerh <info@hammer.bz>
2014-08-22 09:57:25 +0200

    oops, forgot version

--- Popup_chat.user.js
+++ Popup_chat.user.js
@@ -3,7 +3,7 @@
 // @namespace   ssm
 // @description Instead of an in screen chat, pop it allllll up!
 // @include     http://*.grepolis.*/game*
-// @version     0.3.3
+// @version     0.3.4
 // @grant       none
 // @downloadURL https://github.com/breakerh/Popup-Chat/raw/master/Popup_chat.user.js
 // @updateURL https://github.com/breakerh/Popup-Chat/raw/master/Popup_chat.meta.js
